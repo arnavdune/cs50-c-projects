e7be50e6863537150264ff7ef588bdca36b63529
Arnav Sagar <arnavdune@gmail.com>
2026-01-09 13:39:27 +0530

    Fix logic error of word count

--- lec2/textAnalyzer.c
+++ lec2/textAnalyzer.c
@@ -6,7 +6,9 @@
 int main(void)
 {
     string prompt = get_string("Prompt: ");
-    int wordCount = 1;
+    int spaceCount = 0;
+    int wordCount = 0;
+    bool inWord = false;
     int letterCount = 0;
     int frequency[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0, times = strlen(prompt); i < times; i++)
@@ -14,14 +16,19 @@ int main(void)
         char letter = tolower(prompt[i]);
         if (letter == ' ')
         {
-            wordCount++;
+            spaceCount++;
         }
         else if (isalpha(letter))
         {
             letterCount++;
-            frequency[(letter - 97)]++; // if letter is a (ascii - 97) then it will increase the frequency[97 - 97] by one 
+            frequency[letter - 97]++; // if letter is a (ascii - 97) then it will increase the frequency[97 - 97] by one 
+            if (!inWord){
+                wordCount++;
+                inWord = true;
+            }
         }
     }
+    
     // Results:-
     printf("Word count: %d\n", wordCount);
     printf("Letter count: %d\n", letterCount);
